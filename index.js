df213c52ffc5fe0f53891e65ca20058ea6a2b2f6
CatherineHonors <cathonors@gmail.com>
2022-04-04 16:57:49 -0400

    commit tree 36401ef72ac6f74961244dff2e08555efe79cfad

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(amountFeet){
+  if (amountFeet > 2500) {
+    return "No can do.";
+  } else if (amountFeet < 2500 && amountFeet > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (amountFeet <= 2000 && amountFeet > 400) {
+    return "That will be twenty bucks.";
+  } else if (amountFeet <= 400) {
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  if (city == "NYC"){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(amount){
+  if (amount == "generous"){
+    return "Thank you so much.";
+  } else if (amount == "not as generous"){
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
